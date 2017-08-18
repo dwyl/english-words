1028ac5043809512cd241914e41a220d618a3cd0
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2017-08-18 09:08:36 +0600

    fixed typo english

--- read_english_dictionary.py
+++ read_english_dictionary.py
@@ -4,8 +4,8 @@ import os, sys
 def load_words():
     try:
         filename = os.path.dirname(sys.argv[0])+"\\"+"words_dictionary.json"
-        with open(filename,"r") as enlish_dictionary:
-            valid_words = json.load(enlish_dictionary)
+        with open(filename,"r") as english_dictionary:
+            valid_words = json.load(english_dictionary)
             return valid_words
     except Exception as e:
         return str(e)
