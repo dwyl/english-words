eb3cc0b1291d03c4d49b5d30870869fc5170d3e2
KarjaKAK <kakkarja.github@gmail.com>
2018-02-28 10:13:25 +0700

    Look for Words within a Word
    Searching words that valid in English-words.txt that contain letters from the word that you search.

---
+++ look_w.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 13:48:31 2018
+
+@author: karja
+"""
+
+import itertools, os
+os.chdir("C:\\Users\\karja")
+
+# Open words.txt
+wor = open("words.txt","r")
+wor = [x[:-1] for x in wor]
+
+# Looking words that in the words list
+# by number of letters and alphabets
+def look_w(word,num):
+    
+    # Getting words list according to number of letters 
+    mx = [wor[x] for x in range(len(wor)) if len(wor[x]) == num] 
+    
+    # Create list of words that using itertools from the letters
+    # and listing all words that relate to the word's letters.
+    a = []
+    b = {word[c]:word.count(word[c]) for c in range(len(word))}
+    for ele in  itertools.product(word,repeat = num):
+        d = {ele[c]:ele.count(ele[c]) for c in range(len(ele))}
+        z=[]
+        for i in d:
+            try:
+                v = len(d)
+                if d[i] == b[i]:
+                    z.append('ok')
+            except:
+                pass
+            finally:
+                if v == len(z):
+                    a.append("".join(ele))
+    
+    # Matching the created list words with the words in words.txt
+    # and gather the the valid words
+    a = [a[i] for i in range(len(a)) if a[i] in wor or a[i].capitalize() in wor]
+    return a
