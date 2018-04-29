0c183319b285b45549364d2dbf87f85939828874
Stack-of-Pancakes <angrybisquick@gmail.com>
2018-04-29 14:47:01 -0400

    Simplify looking for similar words
    Saw your pull request: https://github.com/dwyl/english-words/pull/38
    Wanted to show you there was a simpler way to accomplish what you
    were doing.  Also this solution returns words in the original case.

--- look_w.py
+++ look_w.py
@@ -1,46 +1,16 @@
-import itertools, os
-from pathlib import Path
+# Open words.txt.  Assumes words.txt is in same folder as look_w.py
+words = open('words.txt').read().split()
 
-# Geting users home directory
-h_path = str(Path.home()) # + any addtional path to a folder that contain word.txt
 
-# Change directory to h_path where words.txt is located
-os.chdir(h_path)
+def look_w(word, num):
+    # Looking words that in the words list
+    # by number of letters and alphabets
+    return [w for w in words if len(w) == num and
+            all(w.lower().count(c) <= word.lower().count(c) for c in w.lower())]
 
-# Open words.txt
-wor = open("words.txt","r")
-wor = [x[:-1] for x in wor]
 
-# Looking words that in the words list
-# by number of letters and alphabets
-def look_w(word,num):
-    
-    # Checking total num against the total letters of word.
-    if num <= len(word):
-        
-        # Getting words list according to number of letters 
-        mx = [wor[x] for x in range(len(wor)) if len(wor[x]) == num]
-        
-        # Listing all words that relate to the word's letters.
-        a = []
-        b = {word[c]:word.count(word[c]) for c in range(len(word))}
-        for ele in  mx:
-            elec = ele.lower()
-            d = {elec[c]:elec.count(elec[c]) for c in range(len(elec))}
-            z=[]
-            for i in d:
-                try:
-                    v = len(d)
-                    if d[i] <= b[i]:
-                        z.append('ok')
-                except:
-                    pass
-                finally:
-                    if v == len(z):
-                        a.append("".join(elec))
-        
-        # Sorted the words list
-        a = sorted(list(set(a)))
-        return a
-    else:
-       return "⚔ Exceeding total letters ⚔".upper() 
+# Usage
+print(look_w('insane', 6))  # prints ['inanes', 'insane', 'sienna']
+print(look_w('quit', 3))    # prints ['ITU', 'qui', 'Tiu', 'tui', 'UIT', 'uti']
+print(look_w('quit', 4))    # prints ['quit']
+print(look_w('cake', 99))   # prints []
